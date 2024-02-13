abd40a2f2012759c4c69fe1bd7d97c0a4dcd6d1b
D-oxys <sanuzumaki12@gmail.com>
2024-02-13 19:12:27 +0700

    add api

---
+++ src/app/api/news/route.ts
@@ -0,0 +1,23 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import axios from 'axios';
+
+const API_KEY = '82c79a6612464ffb81a6ec58c24cb404';
+const BASE_URL = 'https://newsapi.org/v2/top-headlines';
+
+export async function get(req: NextApiRequest, res: NextApiResponse) {
+  const country = req.query.country || 'us';
+
+  try {
+    const response = await axios.get(BASE_URL, {
+      params: {
+        country: country,
+        apiKey: API_KEY,
+        pageSize: 6,
+      },
+    });
+
+    res.status(200).json(response.data.articles);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching news' });
+  }
+}
--- src/services/news.ts
+++ src/services/news.ts
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
-const API_KEY = '82c79a6612464ffb81a6ec58c24cb404';
-const BASE_URL = 'https://newsapi.org/v2';
+// Ganti BASE_URL dengan URL ke serverless function Anda
+const BASE_URL = 'https://kasa-news.vercel.app/api/news';
 
 export const getTopHeadlines = async (country = 'us') => {
   try {
-    const response = await axios.get(`${BASE_URL}/top-headlines?country=${country}&apiKey=${API_KEY}&pageSize=6`);
+    // Karena logika untuk menambahkan API key dan parameter lainnya sudah dipindahkan ke serverless function,
+    // Anda hanya perlu memanggil endpoint tersebut tanpa parameter tambahan
+    const response = await axios.get(`${BASE_URL}`);
     return response.data.articles;
   } catch (error) {
     throw error;
