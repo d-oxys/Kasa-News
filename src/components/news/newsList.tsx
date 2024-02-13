d23bb9082c039c1b508a0f77e9dbe671dfcdf751
D-oxys <sanuzumaki12@gmail.com>
2024-02-13 18:41:44 +0700

    fix news headline

--- src/components/news/newsList.tsx
+++ src/components/news/newsList.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { useNewsState } from '@/store/newsContext';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -17,36 +18,39 @@ const NewsList = () => {
           </div>
           <div className='mt-8 grid grid-cols-1 gap-y-5 md:grid-cols-2 md:gap-x-12'>
             <div className='main'>
-              {headlines[1] && (
-                <Link href={`/artikel/${headlines[1].title}`}>
+              {headlines[0] && headlines[0].urlToImage && (
+                <Link href='/workout/ocQKBz9CZM2CvgKVqTSd'>
                   <Card className='h-full border-2 border-gray-200 p-1'>
                     <div className='relative h-[250px] w-full sm:h-[300px] lg:h-[350px] xl:h-[400px]'>
-                      <Image src={headlines[1].urlToImage} alt='gambar artikel' fill className='object-cover' sizes='(max-width: 768px) 40vw, (max-width: 1200px) 50vw, 60vw' />
+                      <img src={headlines[0].urlToImage} alt='gambar artikel' className='object-cover' style={{ width: '100%', height: '100%' }} sizes='(max-width: 768px) 40vw, (max-width: 1200px) 50vw, 60vw' />
                     </div>
 
-                    <h3 className='hover:text-primary-2 line-clamp-2 text-xl font-bold leading-snug duration-500'>{headlines[1].title}</h3>
-                    <p className='text-primary-3 line-clamp-3 text-sm'>{headlines[1].description}</p>
+                    <h3 className='hover:text-primary-2 line-clamp-2 text-xl font-bold leading-snug duration-500'>{headlines[0].title}</h3>
+                    <p className='text-primary-3 line-clamp-3 text-sm'>{headlines[0].description}</p>
                   </Card>
                 </Link>
               )}
             </div>
             <div className='aside flex flex-col space-y-4'>
-              {headlines.slice(2).map((headlineItem: Headline, k: number) => (
-                <Link href={`/artikel/${headlineItem.title}`} key={k}>
-                  <Card className='border-2 border-gray-200 p-1'>
-                    <div className='flex flex-col md:flex-row md:space-x-3'>
-                      <div className='relative h-[250px] w-full bg-red-300 md:h-[130px] md:w-[40%] xl:h-[150px]'>
-                        <Image src={headlineItem.urlToImage} alt='gambar artikel' sizes='(max-width: 768px) 40vw, (max-width: 1200px) 50vw, 60vw' fill className='object-cover' />
-                      </div>
+              {headlines.slice(1).map(
+                (headlineItem: Headline, k: number) =>
+                  headlineItem.urlToImage && (
+                    <Link href={`/artikel/${headlineItem.title}`} key={k}>
+                      <Card className='border-2 border-gray-200 p-1'>
+                        <div className='flex flex-col md:flex-row md:space-x-3'>
+                          <div className='relative h-[250px] w-full bg-red-300 md:h-[130px] md:w-[40%] xl:h-[150px]'>
+                            <img src={headlineItem.urlToImage} alt='gambar artikel' style={{ width: '100%', height: '100%' }} className='object-cover' />
+                          </div>
 
-                      <div className='mt-5 md:mt-0 md:w-[60%]'>
-                        <h3 className='hover:text-primary-2 line-clamp-2 text-xl font-bold leading-snug duration-500'>{headlineItem.title}</h3>
-                        <p className='mt-4 line-clamp-3 text-sm text-[#637381]'>{headlineItem.description}</p>
-                      </div>
-                    </div>
-                  </Card>
-                </Link>
-              ))}
+                          <div className='mt-5 md:mt-0 md:w-[60%]'>
+                            <h3 className='hover:text-primary-2 line-clamp-2 text-xl font-bold leading-snug duration-500'>{headlineItem.title}</h3>
+                            <p className='mt-4 line-clamp-3 text-sm text-[#637381]'>{headlineItem.description}</p>
+                          </div>
+                        </div>
+                      </Card>
+                    </Link>
+                  )
+              )}
             </div>
           </div>
         </div>
